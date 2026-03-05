6eea09ad0f0e3bbcbb46e4bb052a2ec05156e186
koftamainee <koftamainee@gmail.com>
2026-03-05 22:49:11 +0300

    fix right shift

--- src/mpn/rshift.c
+++ src/mpn/rshift.c
@@ -2,7 +2,7 @@
 
 mp_limb_t mpn_rshift(mp_ptr rp, const mp_limb_t *xp, mp_size_t n, unsigned long int shift) {
   if (shift == 0) {
-    for (mp_size_t i = 0; i < n; i++) {
+    for (mp_size_t i = n - 1; i >= 0; i--) {
       rp[i] = xp[i];
     }
     return 0;
@@ -13,7 +13,7 @@ mp_limb_t mpn_rshift(mp_ptr rp, const mp_limb_t *xp, mp_size_t n, unsigned long 
 
   mp_limb_t carry = 0;
 
-  for (mp_size_t i = 0; i < n; i++) {
+  for (mp_size_t i = n - 1; i >= 0; i--) {
     mp_limb_t temp = xp[i];
     rp[i] = (temp >> shift) | carry;
     carry = temp << lshift;
